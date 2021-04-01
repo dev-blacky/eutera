a7db78994ea0a8a66712ca62c5cea13b81ec99f9
Whiteline <48377708+xWhiteline@users.noreply.github.com>
2021-04-01 22:35:18 +0300

    removed Eutera source code.

--- src/commands/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'ping',
-    aliases: ['pi'],
-    category: 'debug',
-    execute (message) {
-        message.reply(`Average ping: ${Math.round(message.client.ws.ping)} ms`).catch(console.error);
-    }
-};
